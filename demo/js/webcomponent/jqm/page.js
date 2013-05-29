e63ac2a460e951d6ce07e978512bb624d5d793a5
Marty <marty.shu@gmail.com>
2013-05-29 09:06:51 +0800

    add loadCss function in <page> component

--- demo/js/webcomponent/jqm/page.js
+++ demo/js/webcomponent/jqm/page.js
@@ -1,3 +1,11 @@
+function loadCss(url) {
+    var link = document.createElement("link");
+    link.type = "text/css";
+    link.rel = "stylesheet";
+    link.href = url;
+    document.getElementsByTagName("head")[0].appendChild(link);
+}
+loadCss('js/jquerymobile/jquery.mobile-1.3.1.min.css');
 // define module (Component)
 define(['gk'], function () {
     return {
